6c2242344307e9fab6e1d9f7394c4103314fb86f
zch-alax <aa0404@outlook.com>
2024-06-02 23:33:38 +0800

    完善游戏，添加音乐

--- Dodge the Creeps!/scripts/Main.gd
+++ Dodge the Creeps!/scripts/Main.gd
@@ -4,12 +4,6 @@ extends Node
 @export var mob_scene: PackedScene
 var score
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	new_game()
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
@@ -18,15 +12,23 @@ func _process(delta):
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUB.game_over()
+	$Music.stop()
+	$DeathSound.play()
 
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
-	
+	$HUB.update_score(score)
+	$HUB.show_message("Get Ready")
+	# call_group() 函数调用组中每个节点上的删除函数——让每个怪物删除其自身
+	get_tree().call_group("mobs", "queue_free")
+	$Music.play()
 
 func _on_score_timer_timeout():
 	score += 1
+	$HUB.update_score(score)
 
 
 func _on_start_timer_timeout():
