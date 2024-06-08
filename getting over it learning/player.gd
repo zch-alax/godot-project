cd212b58d238592a1c218e014b474fb192a95895
zch-alax <aa0404@outlook.com>
2024-06-08 11:10:40 +0800

    update

--- getting over it learning/player.gd
+++ getting over it learning/player.gd
@@ -2,6 +2,8 @@ class_name Player
 
 extends CharacterBody2D
 
+const SPEED = 200
+
 var mouse_pos = Vector2.ZERO
 var look_dir = 0
 # 默认重力 980
@@ -10,10 +12,14 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 func _physics_process(delta):
 	mouse_pos = get_global_mouse_position() # 获得全局的鼠标坐标
 	# normalized() 归一化 将向量大小归一化为1
-	var player_pos = global_position
-	var pos = mouse_pos - player_pos
-	look_dir = pos.normalized().angle() + deg_to_rad(90)
+	#var player_pos = global_position
+	#var pos = mouse_pos - player_pos
+	#look_dir = pos.normalized().angle() + deg_to_rad(90)
+	look_dir += Input.get_axis("ui_left", "ui_right") * deg_to_rad(5)
 	rotation = lerp_angle(rotation, look_dir, 1.0) # 让角色旋转，能转起镐子
+	
+	if is_on_floor():
+		velocity.x = Input.get_axis("move_left", "move_right") * SPEED
 	
 	if not is_on_floor():
 		velocity.y += gravity * delta
