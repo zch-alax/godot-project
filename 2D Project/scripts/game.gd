cb24c972ad1ce2bbb659fd22aefe9ce9525ac265
zch-alax <AA0404@outlook.com>
2024-06-11 13:00:03 +0800

    update

--- 2D Project/scripts/game.gd
+++ 2D Project/scripts/game.gd
@@ -3,6 +3,9 @@ extends Node2D
 @onready var path_follow = %PathFollow2D
 @onready var game_over = %GameOver
 
+func _ready():
+	$GameOver.hide()
+
 func spawn_mob():
 	var new_mob = preload("res://scenes/mob.tscn").instantiate()
 	path_follow.progress_ratio = randf()
@@ -11,9 +14,18 @@ func spawn_mob():
 
 
 func _on_timer_timeout():
-	spawn_mob()
+	if get_tree().get_nodes_in_group("mobs").size() < 30:
+		spawn_mob()
 
 
 func _on_player_health_depleted():
 	game_over.visible = true
 	get_tree().paused = true
+
+
+func _on_restart_button_pressed():
+	get_tree().reload_current_scene()
+	
+
+func _on_quit_button_pressed():
+	get_tree().quit()
