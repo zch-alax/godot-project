268dd4d251f84ffae4cdc3339a0889ce0f086ea1
zch-alax <aa0404@outlook.com>
2025-01-08 18:43:57 +0800

    删除alice项目

--- alice-physic-trial/scripts/chapter_1.gd
+++
@@ -1,4 +0,0 @@
-extends Node2D
-
-func _ready() -> void:
-	Dialogic.start("Chapter1")
--- alice-physic-trial/scripts/game.gd
+++
@@ -1,8 +0,0 @@
-extends Node2D
-
-@onready var main_menu: CanvasLayer = $MainMenu
-@onready var chapters: Node2D = $Chapters
-	
-
-func _ready() -> void:
-	Main.game = self
